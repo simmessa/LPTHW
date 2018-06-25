def37706fac3296050c61414ff4fb5f6bead5062
Simone Messaggi <simmessa@ilgiornale-web.it>
2018-06-25 16:45:37 +0200

    Sooooo slow, this course

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# Comments
+# Thanks god #s work on Python
+
+print "I love this style. OMG!" # but comment I HATE THIS
+
+# Another comment, life is great
+# print "Please kill me LPTHW"
+
+print "This is the end of the World."
