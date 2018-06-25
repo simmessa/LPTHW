03af32e77f5f427245956143eccb36cfb76a71da
Simone Messaggi <simmessa@ilgiornale-web.it>
2018-06-25 16:36:39 +0200

    Drills, don't you love them?

---
+++ ex1_drills.py
@@ -0,0 +1,9 @@
+#print "Hello World!"
+#print "Hello banana"
+#print "I like typing this, but bananas, as well."
+#print "Not that kind of bananas, btw..."
+#print 'Yay!'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+
+print "This is just mine, a personal addition to strings."
