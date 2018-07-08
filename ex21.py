669dd009cae4356b21f2e7b0645682c4b0645c9f
Simone Messaggi <simmessa@gmail.com>
2018-07-08 19:13:08 +0200

    Building stamina

---
+++ ex21.py
@@ -0,0 +1,27 @@
+# @SM functions return stuff, yep
+
+def add(a,b):
+    print "adding %d and %d" % (a,b)
+    return a + b
+
+def subtract(a,b):
+    print "subtracting %d - %d" % (a,b)
+    return a - b 
+
+def multiply(a,b):
+    print "multiplying %d by %d" % (a,b)
+    return a * b
+
+def divide(a,b):
+    print "divide %d by %d" % (a,b)
+    return a / b
+
+print "Let's do my by functions"
+
+ass = add(30,5)
+heiny = subtract(30,5)
+anus = multiply(30,5)
+bunghole = divide (30,5)
+
+print "Ass: %d, Heiny: %d, anus: %d, bunghole: %d" % (ass, heiny, anus, bunghole)
+
---
+++ ex24.py
@@ -0,0 +1,36 @@
+# @SM building stamina in python?
+
+print "Practice shit"
+print 'Tab me\t and then return me\n but don\'t \\ double back slash me'
+
+poem = """
+A Poem
+don't you like it?
+It's your fault, nasty scumbag
+"""
+
+print "---------"
+print poem
+print "@@@@@@@@@"
+
+five = 10 - 2 + 3 - 6
+
+print "five is actually %d" % five
+
+def stupid(starter):
+    banana = starter**starter
+    cream = banana / starter
+    prot = banana + cream
+    return banana, cream, prot
+
+thousand = 10
+
+a,b,c = stupid(thousand)
+
+print "when I start at %d" % thousand
+print "After stupid all I get is: %d, %d, %d" % (a,b,c)
+
+#thousand /= thousand
+
+print "Another way of stupid is: %d, %d, %d" % stupid(thousand)
+
