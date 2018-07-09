4a1c4092b4543f06be6493e05f2ee2149af56c5f
Simone Messaggi <simmessa@ilgiornale-web.it>
2018-07-09 14:14:38 +0200

    Added back old ex9 and ex10

---
+++ ex9.py
@@ -0,0 +1,13 @@
+# @SM More strings I'm gonna throw up
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "The days: ", days
+print "The months: ", months
+
+print """
+Don't you love triple double dash?
+I don't
+Actually, I hate'em
+"""
