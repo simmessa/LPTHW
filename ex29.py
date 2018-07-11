9967828c0ae81cf0e0fcbe4f3d3de05887d5e4f0
Simone Messaggi <simmessa@ilgiornale-web.it>
2018-07-11 19:07:06 +0200

    If and else, what else?

---
+++ ex29.py
@@ -0,0 +1,28 @@
+# @SM The mighty IF statement
+
+pippe = 20
+caz = 30
+dongs = 15
+
+if pippe < caz:
+    print "Too many caz, to few pippe"
+
+if pippe > caz:
+    print "Pay attention or you will consume yours..."
+
+if pippe < dongs:
+    print "Your rear end will have fun surely"
+
+if pippe > dongs:
+    print "More fun to the front end!"
+
+dongs += 5
+
+if pippe >= dongs:
+    print "You might choose either for fun"
+
+if pippe <= dongs:
+    print "It's nice to have choice"
+
+if pippe == dongs:
+    print "You sure had a busy day..."
---
+++ ex30.py
@@ -0,0 +1,25 @@
+# @SM what else?
+
+pippe = 30
+caz = 40
+busi = 15
+
+if caz > pippe:
+    print "Why don't you try one of the busi?"
+elif caz < pippe:
+    print "No busi to fick"
+else:
+    print "This doesn't work, Zak"
+
+if busi > caz:
+    print "You are in good company"
+elif busi < caz:
+    print "Try not to bend, it won't be pleasant"
+else:
+    print "Dunno, go away please"
+
+if pippe > busi:
+    print "You like to play with yourself don't you?"
+else:
+    print "Try to take in some action"
+    
